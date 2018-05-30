2f55ac51dd72e1f0d293ecff275757b1d32aef81
hanxiaoqing <han@github.com>
2018-05-30 19:00:39 +0800

    commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var db = require("./model/db.js");
 var encodeParams = require("./model/encodeParams");
 
@@ -6,9 +5,9 @@ var encodeParams = require("./model/encodeParams");
 // 客户端请求接口的时候 localstorage有acclogin_token值就传，若无就传""，
 
 var userLoginParams = {
-    "acclogin_token" : "xxxxx",
-    "loginName" : "han",
-    "password" : 23,
+    "acclogin_token": "xxxxx",
+    "loginName": "han",
+    "password": 23,
     "res_action": "login",
 };
 
@@ -20,11 +19,11 @@ console.log(encodeParams.encodeParams(userLoginParams));
 
 
 var loginResultInfo = {
-    "userid" : "1",
-    "userName" : "han",
-    "acclogin_token" : "xxxxx",
-    "error_num" : "1",
-    "error_msg" : "xxxx"
+    "userid": "1",
+    "userName": "han",
+    "acclogin_token": "xxxxx",
+    "error_num": "1",
+    "error_msg": "xxxx"
 }
 
 // 通过error_num判断失败类型，若是login_token过期，就一定要清localstorage的acclogin_token为""，
@@ -51,5 +50,3 @@ var loginResultInfo = {
 // db.mg_find("users", {"password" : {$gt : 2}}, {limit: 2}, (err,result) => {
 //     console.log(result);
 // });
-
-
--- model/encodeParams.js
+++ model/encodeParams.js
@@ -3,11 +3,11 @@ var MD5 = require("./encodeString").MD5;
 
 function signString(parms, appkey) {
     //console.log(typeof(parms));
-    if (typeof(parms) != "object") {
+    if (typeof(parms) !== "object") {
         return null;
     }
     var paramsKeys = Object.keys(parms);
-    if (paramsKeys.length == 0) {
+    if (paramsKeys.length === 0) {
         return null;
     }
     var connectParamString = '';
@@ -18,14 +18,13 @@ function signString(parms, appkey) {
     return MD5(signString);
 };
 
+
 function encodeParams(parms) {
     var sign_str = signString(parms, "tfdajfsdad");
     parms["time_stamp"] = new Date().getTime();
     parms["sign_str"] = sign_str;
     return parms;
 }
-
-
 
 exports.encodeParams = encodeParams;
 exports.signString = signString;
